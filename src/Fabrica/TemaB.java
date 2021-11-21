9e5375eb7ad2fae3d4bd901e88a59cff4235f4fa
ezearce <ezequiel.arce@hotmail.com>
2021-11-20 21:04:20 -0300

    Actualizacion TemaB

--- src/Fabrica/TemaB.java
+++ src/Fabrica/TemaB.java
@@ -37,7 +37,9 @@ public class TemaB extends Tema{
 		
 		protagonista = new Protagonista(miJuego);
 		protagonista.getGUI().setNormal("/imagenesB/barco_arriba.png", "/imagenesB/barco_abajo.png", "/imagenesB/barco_der.png", "/imagenesB/barco_izq.png");
-		
+		protagonista.getGUI().setAzul("/imagenesB/barcoAzul_arriba.png", "/imagenesB/barcoAzul_abajo.png", "/imagenesB/barcoAzul_derecha.png", "/imagenesB/barcoAzul_izquierda.png");
+		protagonista.getGUI().setBomba("/imagenesB/barcoBomba_arriba.png", "/imagenesB/barcoBomba_abajo.png", "/imagenesB/barcoBomba_derecha.png", "/imagenesB/barcoBomba_izquierda.png");
+		protagonista.getGUI().setAzul("/imagenesB/barcoVelocidad_arriba.png", "/imagenesB/barcoVelocidad_abajo.png", "/imagenesB/barcoVelocidad_derecha.png", "/imagenesB/barcoVelocidad_izquierda.png");
 		return protagonista;
 	}
 
@@ -61,44 +63,53 @@ public class TemaB extends Tema{
 
 	@Override
 	public Velocidad getVelocidad() {
-		// TODO Auto-generated method stub
-		return null;
+		return new Velocidad("/imagenesB/barril.png");
 	}
 
 	@Override
 	public PowerPellet getPowerPellet() {
-		// TODO Auto-generated method stub
-		return null;
+		return new PowerPellet("/imagenesB/cofre.png");
 	}
 
 	@Override
 	public PacDot getPacDot() {
-		// TODO Auto-generated method stub
-		return null;
+		return new PacDot("/imagenesB/pacdotB");
 	}
 
 	@Override
 	public Blinky getBlinky() {
-		// TODO Auto-generated method stub
-		return null;
+		Blinky enemigo;
+		enemigo=new Blinky("/imagenesB/blinky_abajo.png");
+		enemigo.getGUI().setNormal("/imagenesB/blinky_abajo.png","/imagenesB/blinky_arriba.png","/imagenesB/blinky_izquierda.png","/imagenesB/blinky_derecha.png") ;
+		enemigo.getGUI().setAzul("/imagenesB/barcoPoliciaAzul_abajo.png", "/imagenesB/barcoPoliciaAzul_arriba.png", "/imagenesB/barcoPoliciaAzul_izquierda.png", "/imagenesB/barcoPoliciaAzul_derecha.png");
+		return enemigo;
 	}
 
 	@Override
 	public Inky getInky() {
-		// TODO Auto-generated method stub
-		return null;
+		Inky enemigo;
+		enemigo=new Inky("/imagenesB/inky_abajo.png");
+		enemigo.getGUI().setNormal("/imagenesB/inky_abajo.png","/imagenesB/inky_arriba.png","/imagenesB/inky_izquierda.png","/imagenesB/inky_derecha.png") ;
+		enemigo.getGUI().setAzul("/imagenesB/barcoPoliciaAzul_abajo.png", "/imagenesB/barcoPoliciaAzul_arriba.png", "/imagenesB/barcoPoliciaAzul_izquierda.png", "/imagenesB/barcoPoliciaAzul_derecha.png");
+		return enemigo;
 	}
 
 	@Override
 	public Pinky getPinky() {
-		// TODO Auto-generated method stub
-		return null;
+		Pinky enemigo;
+		enemigo=new Pinky("/imagenesB/pinky_abajo.png");
+		enemigo.getGUI().setNormal("/imagenesB/pinky_abajo.png","/imagenesB/pinky_arriba.png","/imagenesB/pinky_izquierda.png","/imagenesB/pinky_derecha.png") ;
+		enemigo.getGUI().setAzul("/imagenesB/barcoPoliciaAzul_abajo.png", "/imagenesB/barcoPoliciaAzul_arriba.png", "/imagenesB/barcoPoliciaAzul_izquierda.png", "/imagenesB/barcoPoliciaAzul_derecha.png");
+		return enemigo;
 	}
 
 	@Override
 	public Clyde getClyde() {
-		// TODO Auto-generated method stub
-		return null;
+		Clyde enemigo;
+		enemigo=new Clyde("/imagenesB/inky_abajo.png");
+		enemigo.getGUI().setNormal("/imagenesB/clyde_abajo.png","/imagenesB/clyde_arriba.png","/imagenesB/clyde_izquierda.png","/imagenesB/clyde_derecha.png") ;
+		enemigo.getGUI().setAzul("/imagenesB/barcoPoliciaAzul_abajo.png", "/imagenesB/barcoPoliciaAzul_arriba.png", "/imagenesB/barcoPoliciaAzul_izquierda.png", "/imagenesB/barcoPoliciaAzul_derecha.png");
+		return enemigo;
 	}
 
 	@Override
